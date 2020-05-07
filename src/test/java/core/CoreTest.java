391c2edd86e1ab22925a91f01f364ba24003d306
Iurii Lezhenin <yurij.lezhenin@gmail.com>
2020-05-07 12:15:53 +0300

    add first test and maven pom.xml

---
+++ src/test/java/core/CoreTest.java
@@ -0,0 +1,14 @@
+package core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CoreTest {
+
+    @Test
+    void test(){
+        assertEquals(0, 0);
+    }
+
+}
